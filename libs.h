1de0bfc819c32f64d1f6b213aaca1bc4d968d893
Robert Hammond <robert.hammond@datasat.com>
2023-02-25 21:09:55 +0000

    rm pThread file

--- libs.h
+++ libs.h
@@ -1,6 +1,13 @@
 #include <iostream>
 
-int main()
+class Libs
 {
-    
-}
+    private:
+
+    public:
+
+    inline void printThings()
+    {
+        std::cout << "Stuff and things" << std::endl;
+    }
+};
